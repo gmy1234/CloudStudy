b969e0ff3d87ae6bb43dced7cbc8bf7f8760a601
gmy <1508594767@qq.com>
2022-07-22 16:59:10 +0800

    添加网关Gateway

---
+++ cloud-gateway-9257/src/main/java/com/gmy/cloud/Gateway.java
@@ -0,0 +1,18 @@
+package com.gmy.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gmydl
+ * @title: Gateway
+ * @projectName CloudStudy
+ * @description: 网管理
+ * @date 2022/7/22 16:16
+ */
+@SpringBootApplication
+public class Gateway {
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway.class);
+    }
+}
